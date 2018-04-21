1c90cdc60588ed07d3d9d64a1ab354a3cfbb0f9c
DESKTOP-OM343O8\Agnieszka <agnieszka.t1994@gmail.com>
2018-04-21 12:17:29 +0200

    menu, log in, create account

---
+++ www/js/script.js
@@ -0,0 +1,29 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+$(document).ready(function(){
+	$('.menu-mobile').on('click touchstart', function(e){
+		$('html').toggleClass('menu-active');
+	  	e.preventDefault();
+	});
+})
+
+$(function() {
+  $("#listView li").click(function () {
+    if ( $("#listView li").hasClass("list-item-active") ) {
+      $("#listView li").removeClass("list-item-active");
+    }
+    $(this).addClass("list-item-active");
+  });
+});
+
+
+var c = document.querySelector('.card'), 
+    switchers = c.querySelectorAll('.switch');
+
+for(var i = 0; i < switchers.length; i++) {
+  switchers[i].addEventListener('click', function(){
+    c.classList.toggle('flipped');
+  }, false);
+}
